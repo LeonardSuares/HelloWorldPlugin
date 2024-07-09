95b04803a1efceb9cf361f609ca1721568f853b1
LeonardSuares <leonard.suares@gmail.com>
2024-07-09 12:16:09 -0400

    Add project files.

---
+++ HelloWorldPlugin/HelloWorldPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldPlugin
+{
+    public class HelloWorldPlugin
+    {
+        public void RunAction()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ HelloWorldPlugin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize the plugin
+            HelloWorldPlugin plugin = new HelloWorldPlugin();
+
+            //Execute the action
+            plugin.RunAction();
+        }
+    }
+}
